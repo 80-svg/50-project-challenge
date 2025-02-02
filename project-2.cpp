9b19fa3cbd219a4f7711b8271d1911118abacb52
Makis Laskaris <makislaskaris@gmail.com>
2025-02-02 11:30:11 +0000

    Make Player Input

---
+++ project-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::cout << "Player 1, ender what you want:";
+    std::string input1;
+    std::cin >> input1;
+    std::cout << "Player 2, ender what you want:";
+    std::string input2;
+    std::cin >> input2;
+    
+    return 0; 
+}
